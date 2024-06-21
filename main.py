cf5cc4df0c5c6d7b759a842ea01dd810854a3099
ZaharPavlikov <94977317+ZaharPavlikov@users.noreply.github.com>
2024-06-21 21:11:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from fastapi import FastAPI, HTTPException, Depends, Request,Form,status
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+async def read_api(request: Request):
+    return templates.TemplateResponse("index.html",{"request": request}) 
